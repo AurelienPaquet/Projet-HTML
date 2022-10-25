9bfe41532e6677602ecdb249d8d15c3b77401d22
ElPaquito <aurelien.paquet@groupe-esigelec.org>
2022-10-25 11:39:49 +0200

    bienvenu

--- head.php
+++ head.php
@@ -9,11 +9,17 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">    <title><?php echo $titre ?></title>
 <!-- Police -->
 
+<link rel="stylesheet" href="https://fonts.google.com/specimen/Anton?sort=popularity">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Abril+Fatface&display=swap" rel="stylesheet">
 
 
 <!-- CSS -->
 
   <link rel="stylesheet" href="css/index.css">
+  <link rel="stylesheet" href="css/footer.css">
+
 
 <!-- Bootstrap -->
 
