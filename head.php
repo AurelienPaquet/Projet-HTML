5fb88e05eb74bc284323583016fadba544ac4e62
ElPaquito <aurelien.paquet@groupe-esigelec.org>
2022-10-25 08:29:00 +0200

    test

---
+++ head.php
@@ -0,0 +1,22 @@
+<?php
+  session_start();
+?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">    <title><?php echo $titre ?></title>
+<!-- Police -->
+
+
+
+<!-- CSS -->
+
+  <link rel="stylesheet" href="css/index.css">
+
+<!-- Bootstrap -->
+
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+</head>
+<body>
--- index.php
+++ index.php
@@ -1,18 +1,111 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Page d'accueil</h1>
+<?php
 
-    <h2>test</h2>
+  $titre = "Page d'accueil";
+  include 'head.php';
 
-    <div class="spinner-border" role="status">
-  <span class="visually-hidden">Loading...</span>
-</div>
-</body>
-</html>
+// Affichage des messages à l'aide de Boostrap 
+// (composant alert)
+  if(isset($_SESSION['message'])) {
+    echo '<div class="alert alert-primary" role="alert">';
+    echo $_SESSION['message'];
+    echo '</div>';
+    unset($_SESSION['message']);
+  }
+?>
+
+<!-- Header -->
+<header class="header">
+  <div class="container-fluid sticky-top">
+    <div class="row">
+        <div class="col" id="logo">
+          <img src="image/logo_esigelec.png" alt="Logo de l'Esigelec">
+        </div>
+        <div class="col-6 text-center" id="menu">
+          <a href="Page Accueil">Accueil</a>
+          <a href="Calendrier">Calendrier</a>
+          <a href="dq">Menu</a>
+        </div>
+        <div class="col" id="icons">
+          <div class="icons">
+            <div class="fas fa-user"></div>
+          </div>
+        </div>      
+    </div>
+  </div>
+</header>
+
+<!-- Fin Header -->
+
+<!--Main-->
+
+<main class="main">
+  <div class="row">
+
+
+
+    <div class="col" id="entree">
+      <div id="carousel-plat" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img src="image/img-menu3.jpg" class="d-block w-100" alt="...">
+          </div>
+          <div class="carousel-item">
+            <img src="image/img-menu2.jpg" class="d-block w-100" alt="...">
+          </div>
+          <div class="carousel-item">
+            <img src="image/img-menu3.jpg" class="d-block w-100" alt="...">
+          </div>
+          <div class="carousel-item">
+            <img src="image/img-menu4.jpg" class="d-block w-100" alt="...">
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col" id="plat">
+      <div id="carousel-plat" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img src="image/img-main1.jpg" class="d-block w-100" alt="...">
+          </div>
+          <div class="carousel-item">
+            <img src="image/img-menu2.jpg" class="d-block w-100" alt="...">
+          </div>
+          <div class="carousel-item">
+            <img src="image/img-menu3.jpg" class="d-block w-100" alt="...">
+          </div>
+          <div class="carousel-item">
+            <img src="image/img-menu4.jpg" class="d-block w-100" alt="...">
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+    <div class="col" id="dessert">
+    <div id="carousel-dessert" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+          <div class="carousel-item">
+            <img src="image/img-menu2.jpg" class="d-block w-100" alt="...">
+          </div>
+          <div class="carousel-item active" data-bs-interval=1000>
+            <img src="image/img-menu3.jpg" class="d-block w-100" alt="...">
+          </div>
+          <div class="carousel-item" data-bs-interval=2000>
+            <img src="image/img-menu4.jpg" class="d-block w-100" alt="...">
+          </div>
+          <div class="carousel-item "data-bs-interval =1500>
+            <img src="image/img-main1.jpg" class="d-block w-100" alt="...">
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</main>
+
+<!--Fin Main-->
+
+<?php 
+  include 'footer.php';
+?> 
+   
