426e3ab2cd5abb0fa9f7ef334334f0f9240cf5d8
ElPaquito <aurelienpaquet2016@gmail.com>
2022-11-13 17:46:02 +0100

    profile rectification

--- profile.php
+++ profile.php
@@ -14,35 +14,56 @@
   }
 ?>
 
+<?php
+ if($_SESSION["userrole"] === 1){
+    echo "<section class='profile'>
+        <div class='row' style='background-color: black; width: 100%; height: 500px;'>
+            <div class='col-12'>
+                <h1 style='color:white;text-align: center;padding: 10px'>NOM : " . $_SESSION['usernom'] . "
+            </h1>
+            </div>
+            <div class='col-12'>
+                <h1 style='color:white;text-align: center;padding: 10px'>PRENOM : " . $_SESSION['userprenom'] . "</h1>
+            </div>
+            <div class='col-12'>
+                <h1 style='color:white;text-align: center;padding: 10px'>EMAIL : " . $_SESSION['useremail'] . "</h1>
+            </div>
+            <div class='col-12'>
+                <h1 style='color:white;text-align: center;padding: 10px'>UTILISATEUR</h1>
+            </div>
+        </div>
+    </section>";
+} else if($_SESSION['userrole'] === 2){
+    echo "<section class='profile'>
+        <div class='row' style='background-color: black; width: 100%; height: 500px;'>
+            <div class='col-12'>
+                <h1 style='color:white;text-align: center;padding: 10px'>NOM : " . $_SESSION['usernom'] . "
+            </h1>
+            </div>
+            <div class='col-12>
+                <h1 style='color:white;text-align: center;padding: 10px'>PRENOM : " . $_SESSION['userprenom'] . "</h1>
+            </div>
+            <div class='col-12'>
+                <h1 style='color:white;text-align: center;padding: 10px'>EMAIL : " . $_SESSION['useremail'] . " </h1>
+            </div>
+            <div class='col-12'>
+                <h1 style='color:white;text-align: center;padding: 10px'>RESPONSABLE</h1>
+            </div>
+        </div>
+    </section>";
+    //ajout de plat
+} else  if($_SESSION["userrole"] === 3){
+    echo "<section class='profile'>
+    <div class='row' style='background-color: black; width: 100%; height: 500px;'>
+        <div class='col-12'>
+            <h1 style='color:white;text-align: center;padding: 10px'>ADMIN</h1>
+        </div>
+    </div>
+</section>";
+//ajout plat + responsable
+}
 
-<section class="profile">
-  <div class="row" style="background-color: black; width: 100%; height: 500px;">
-    <div class="col-12">
-        <h1 style="color:white;text-align: center;padding: 10px">Nom : <?php echo $_SESSION['usernom'];?></h1>
-    </div>
-    <div class="col-12">
-        <h1 style="color:white;text-align: center;padding: 10px">Prénom : <?php echo $_SESSION['userprenom'];?></h1>
-    </div>
-    <div class="col-12">
-        <h1 style="color:white;text-align: center;padding: 10px">Email : <?php echo $_SESSION['useremail']; ?></h1>
-    </div>
-    <div class="col-12">
-        <?php
-            if($_SESSION["userrole"] === 1){
-                echo "<h1 style='color:white;text-align: center;padding: 25px;
-                '>UTILISATEUR</h1>";
-            } else if($_SESSION['userrole'] === 2){
-                echo "<h1 style='color:white;text-align: center;padding: 25px;border-color: .1rem solid rgba(255,255,255,.3);
-                '>RESPONSABLE</h1>";
-            } else if($_SESSION['userrole'] === 3){
-                echo "<h1 style='color:white;text-align: center;padding: 25px;border-color: .1rem solid rgba(255,255,255,.3);
-                '>ADMIN</h1>";
-            }
-        ?>
-    </div>
-  </div>
-
-</section>
+?>
 
 <?php 
   include 'footer.php';
